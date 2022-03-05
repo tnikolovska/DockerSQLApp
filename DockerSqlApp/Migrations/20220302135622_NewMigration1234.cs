1dd1140ab21e58a201369f5e493f8e6397d337b3
Teodora <nikolovskat95@gmail.com>
2022-03-05 13:43:17 +0100

    First commit

---
+++ DockerSqlApp/Migrations/20220302135622_NewMigration1234.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DockerSqlApp.Migrations
+{
+    public partial class NewMigration1234 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
